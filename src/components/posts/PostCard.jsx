9cef7685e0ac64b39a14e5d47bf6a7ee4ec56b2c
anuragshuklajec <anuragshuklajec@gmail.com>
2024-05-06 19:34:48 +0530

    postCard component complete

--- src/components/posts/PostCard.jsx
+++ src/components/posts/PostCard.jsx
@@ -1,11 +1,68 @@
-function PostCard(){
-    return <>
-        <div className="border">
+import { useState } from "react";
+import { Avatar } from "@mui/material";
 
+function PostCard() {
+  const [expanded, setExpanded] = useState(false);
 
+  const toggleExpand = () => {
+    setExpanded(!expanded);
+  };
+
+  const content = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+  repellat repellendus ipsum dolor! Ea magni saepe ex consequuntur
+  dolor? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis, voluptatibus praesentium. Dignissimos rerum quas neque itaque soluta exercitationem facere autem culpa, consectetur at explicabo atque, sequi eligendi excepturi corporis nulla minima quos!`;
+
+  const trimmedContent = content.slice(0, 150);
+  const shouldTrim = content.length > 150;
+
+  return (
+    <>
+      <div className="border border-slate-300 w-full md:w-96 mb-2">
+        <div className="flex p-2">
+          <Avatar sx={{ width: 30, height: 30 }}>AS</Avatar>
+          <span className="flex-1 ml-2">Anurag Shukla</span>
+          <button className="bg-blue-400 hover:bg-blue-500 text-white font-bold rounded w-20 font-sans">
+            Follow
+          </button>
         </div>
-    </>
-}
+        <div className="p-2">
+          <h2 className="text-xl font-bold text-blue-900">How python works ?</h2>
+          <p>{expanded ? content : trimmedContent}</p>
+          {shouldTrim && (
+            <button
+              onClick={toggleExpand}
+              className="text-blue-500 hover:text-blue-700"
+            >
+              {expanded ? "Read Less" : "Read More"}
+            </button>
+          )}
+        </div>
+      </div>
 
+
+      <div className="border border-slate-300 w-full md:w-96 mb-2">
+        <div className="flex p-2">
+          <Avatar sx={{ width: 30, height: 30 }}>SA</Avatar>
+          <span className="flex-1 ml-2">Suyash Agrawal</span>
+          <button className="bg-blue-400 hover:bg-blue-500 text-white font-bold rounded w-20 font-sans">
+            Follow
+          </button>
+        </div>
+        <div className="p-2">
+          <h2 className="text-xl font-bold text-blue-900">How python works ?</h2>
+          <p>{expanded ? content : trimmedContent}</p>
+          {shouldTrim && (
+            <button
+              onClick={toggleExpand}
+              className="text-blue-500 hover:text-blue-700"
+            >
+              {expanded ? "Read Less" : "Read More"}
+            </button>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default PostCard;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,19 @@
 import PostCard from "../components/posts/PostCard";
 
-function HomePage(){
-    return <>
-        <PostCard/>
-    </>
-}
+function HomePage() {
+  return (
+    <>
+      <div className="flex justify-evenly">
+        <div className="hidden md:block">Left Bar</div>
+        <div className="ml-2 mr-2">
+          <PostCard />
+          
+        </div>
 
+        <div className="hidden md:block">Right Bar</div>
+      </div>
+    </>
+  );
+}
 
 export default HomePage;
